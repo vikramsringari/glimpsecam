2fef04e9442fb3d16d43af6bd6c398665f10acf7
jungo3 <jungo@uw.edu>
2018-07-05 23:54:33 -0700

    Updated code for GlimpseCam.py

--- camera/GlimpseCam.py
+++ camera/GlimpseCam.py
@@ -7,58 +7,47 @@ from time import sleep
 import subprocess as sub
 import os
 import glob
-import pexpect
 
 GPIO.setmode(GPIO.BCM)
-#GPIO.setup(26, GPIO.OUT)
 GPIO.setup(14, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 currentState = False
 prevState = False
 picture = True
 
-#Camera turns on
-sub.call(['cd pikrellcam'],shell=True)
-sub.call(['pikrellcam &'],shell=True)
-sub.call(['cd'],shell=True)
+sub.call('/home/pi/pikrellcam/pikrellcam &',shell=True)
 
 while True:
 	currentState = not GPIO.input(14)
 	if (currentState and not prevState):
-		#print('single press, waiting for double')
-		picture = True
-		time.sleep(0.1)
-		endtime = time.time() + 1
-		while time.time() < endtime:
-                    prevState = currentState
-		    currentState = not GPIO.input(14)
-		    if (currentState and not prevState):
-		    	#print('double press')
-		    	picture = False
-		    	time.sleep(0.01)
-			break
-		    prevState = currentState
-		    time.sleep(0.01)
-		if picture:
-			sub.call(['echo "still" > /home/pi/pikrellcam/www/FIFO'],shell=True)
-			time.sleep(1)
-			list_of_stills = glob.glob('/home/pi/pikrellcam/www/media/stills/*')
-			filename = max(list_of_stills, key=os.path.getctime)
-			time.sleep(1)
-			sub.call('/home/pi/Dropbox-Uploader/dropbox_uploader.sh upload '+filename+' /',shell=True)
-			time.sleep(1)
-                        #sub.call(['blueman-sendto --device='+device+' '+filename], shell=True)
-                        #print filename
-		else:
-                        sub.call(['echo "record on 10 3" > /home/pi/pikrellcam/www/FIFO'],shell=True)
+                picture = True
+                time.sleep(0.01)
+                endtime = time.time() + 1
+                while time.time() < endtime:
+                        prevState = currentState
+                        currentState = not GPIO.input(14)
+                        if (currentState and not prevState):
+                                picture = False
+                                time.sleep(0.01)
+                                break
+                        prevState = currentState
+                        time.sleep(0.01)
+                if picture:
+                        sub.call('echo "still" > /home/pi/pikrellcam/www/FIFO',shell=True)
+                        time.sleep(1)
+                        listPictures = glob.glob('/home/pi/pikrellcam/www/media/stills/*')
+                        filename = max(listPictures, key=os.path.getctime)
+                        time.sleep(0.01)
+                        sub.call('/home/pi/Dropbox-Uploader/dropbox_uploader.sh upload '+filename+' /',shell=True)
+                        time.sleep(0.01)
+                else:
+                        sub.call('echo "record on 10 3" > /home/pi/pikrellcam/www/FIFO',shell=True)
                         time.sleep(20)
-			list_of_videos = glob.glob('/home/pi/pikrellcam/www/media/videos/*')
-			filename = max(list_of_videos, key=os.path.getctime)
-			time.sleep(1)
-			sub.call('/home/pi/Dropbox-Uploader/dropbox_uploader.sh upload '+filename+' /',shell=True)
-			time.sleep(1)
-                        #sub.call(['blueman-sendto --device='+device+' '+filename], shell=True)
-			#print filename
-	time.sleep(0.1)
-	prevState = currentState
-
+                        listVideos = glob.glob('/home/pi/pikrellcam/www/media/videos/*')
+                        filename = max(listVideos, key=os.path.getctime)
+                        time.sleep(0.01)
+                       # sub.call('aws s3 cp "'+filename+'" s3://glimpse-cam/')
+                        sub.call('/home/pi/Dropbox-Uploader/dropbox_uploader.sh upload '+filename+' /',shell=True)
+                        time.sleep(0.01)
+        time.sleep(0.01)
+        prevState = currentState
