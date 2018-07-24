a6577658cff005a75a3110abb94b0df3b512b927
Cnovotn <35111277+Cnovotn@users.noreply.github.com>
2018-07-23 22:43:52 -0700

    Update urls.py
    Added more urls for the different pages that are available

--- glimpse_project/apps/glimpse_app/urls.py
+++ glimpse_project/apps/glimpse_app/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     url(r'^userPage$', views.userPage),
     url(r'^godModeCheck$', views.godModeCheck),
     url(r'^godMode$', views.godMode),
-    url(r'^logout$', views.logout),
+    url(r'^viewUserInfoGodMode/(?P<user_num>\d+)$', views.viewUserInfoGodMode),
+    url(r'^viewImage/(?P<match>.+)/$', views.viewImage),
+    url(r'^deleteUser/(?P<user_id>\d+)$', views.deleteUser),
+    url(r'^logout$', views.logout)
 ]  
