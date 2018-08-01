489b00f394a65c01f746ca514454d8e8b53afae0
tianhz2 <40640470+tianhz2@users.noreply.github.com>
2018-07-31 23:57:30 -0700

    Update monitor.sh

--- camera/monitor.sh
+++ camera/monitor.sh
@@ -3,9 +3,9 @@ ifconfig > ip.txt
 aws s3 cp "ip.txt" s3://pi-1/
 while true
 do
-	gpio -g read 4 > battery.log
-	echo "; " >> battery.log
-	uptime -p >> battery.log
-	aws s3 cp "battery.log" s3://pi-1/
+	gpio -g read 4 > battery.csv
+	uptime -p >> battery.csv
+	date >> battery.csv
+	aws s3 cp "battery.csv" s3://pi-1/
 	sleep 60
 done
