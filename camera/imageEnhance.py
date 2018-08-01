f890541107bc25a673eaaad9872405d5bd4a4f3e
Vikram R. Sringari <stripes7@uw.edu>
2018-07-31 21:25:32 -0700

    fixed image proces

--- camera/imageEnhance.py
+++ camera/imageEnhance.py
@@ -1,15 +1,22 @@
 import sys, os
 from PIL import Image
 from PIL import ImageEnhance
+from PIL import ImageFilter
+from PIL import ImageFilter
+from transforms import RGBTransform
 
 def simpleImageEnhance(img, saveImg):
     img1 = Image.open(img)
     converter = ImageEnhance.Color(img1)
-    img2 = converter.enhance(1.5)
+    img2 = converter.enhance(1.0)
     enhancer = ImageEnhance.Sharpness(img2)
     img3 = enhancer.enhance(2.0)
-    img3 = img3.crop((316,58,1596,1018)) 
-    img3.save(saveImg)
+    contrast = ImageEnhance.Contrast(img3)
+    img4 = contrast.enhance(1.75)
+    img4 = img4.crop((316,58,1596,1018)) 
+    img4 = img4.convert('RGB')
+    img4 = RGBTransform().mix_with((100, 80, 100),factor=.30).applied_to(img4)
+    img4.save(saveImg)
 
 if __name__ == "__main__":
     simpleImageEnhance('mountain.jpg', 'enhanced_mountain.jpeg')
