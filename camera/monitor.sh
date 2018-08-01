f3b0365b16ac0bb8fb51bfea7d7a32197da92df9
tianhz2 <40640470+tianhz2@users.noreply.github.com>
2018-07-31 21:07:25 -0700

    Add files via upload

--- camera/monitor.sh
+++ camera/monitor.sh
@@ -1,9 +1,11 @@
-#!/bin/bash
+sleep 30
+ifconfig > ip.txt
+aws s3 cp "ip.txt" s3://pi-1/
 while true
 do
 	gpio -g read 4 > battery.log
 	echo "; " >> battery.log
 	uptime -p >> battery.log
 	aws s3 cp "battery.log" s3://pi-1/
-	sleep 10
+	sleep 60
 done
