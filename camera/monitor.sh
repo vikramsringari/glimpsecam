b8c8534c9d938b44a8cc80e76d40d5297c96e090
tianhz2 <40640470+tianhz2@users.noreply.github.com>
2018-08-07 18:42:21 -0700

    Update monitor.sh

--- camera/monitor.sh
+++ camera/monitor.sh
@@ -6,6 +6,6 @@ do
 	gpio -g read 4 > battery.csv
 	uptime -p >> battery.csv
 	date >> battery.csv
-	aws s3 cp "battery.csv" s3://pi-1/
+	aws s3 cp "battery.csv" s3://pi-1/$(hostname)/
 	sleep 60
 done
