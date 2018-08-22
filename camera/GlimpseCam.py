2264dbdd480bd80251e6b792500043f399aba6a0
Justin <jungo@uw.edu>
2018-08-21 19:10:06 -0700

    new code

--- camera/GlimpseCam.py
+++ camera/GlimpseCam.py
@@ -3,8 +3,6 @@
 import time, datetime, os, glob, socket
 import RPi.GPIO as GPIO
 import subprocess as sub
-import imageEnhance as iE
-import renameFile as rF
 
 currentState = False
 prevState = False
@@ -12,9 +10,7 @@ picture = True
 filename = "file.wav"
 held = False
 
-with open("./glimpsecam/camera/numFile.txt") as numFile:
-	int_list = [int(i) for i in numFile.readline().split()]
-
+sub.call('python /home/pi/glimpsecam/camera/upload.py &',shell=True)
 sub.call('/home/pi/pikrellcam/pikrellcam &',shell=True)
 time.sleep(3)
 
@@ -48,6 +44,10 @@ time.sleep(3)
 #BACK TO RUNNING
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(5, GPIO.OUT)
+GPIO.setup(5, GPIO.HIGH)
+time.sleep(1)
+GPIO.output(5, GPIO.LOW)
 
 while True:
 	currentState = not GPIO.input(12)
@@ -73,33 +73,21 @@ while True:
 				if (not currentState):
 					held = False
 			if held:
+				GPIO.output(5, GPIO.HIGH)
+				time.sleep(1)
+				GPIO.output(5, GPIO.LOW)
 				GPIO.cleanup()
+				sub.call("kill $(ps | grep bash | awk '{print $1}')",shell=True)
 				sub.call('python ./glimpsecam/camera/GlimpseCamLowPowerMode.py &', shell=True)
 				sub.call('pkill -f ./pikrellcam/pikrellcam', shell=True)
 				sub.call('sudo ifconfig wlan0 down', shell=True)
+				sub.call('pkill -f ./glimpsecam/camera/upload.py', shell=True)
 				sub.call('pkill -f ./glimpsecam/camera/GlimpseCam.py', shell=True)
 		if picture:
 			sub.call('echo "still" > /home/pi/pikrellcam/www/FIFO', shell=True)
 			time.sleep(1)
-			filename = rF.rename('/home/pi/pikrellcam/www/media/stills',int_list[0])
-			iE.simpleImageEnhance(filename, filename)
-			int_list[0] -= 1
-			time.sleep(0.01)
 		else:
 			sub.call('echo "record on 5 5" > /home/pi/pikrellcam/www/FIFO', shell=True)
 			time.sleep(10)
-			filename = rF.rename('/home/pi/pikrellcam/www/media/videos',int_list[1])
-			int_list[1] -= 1
-			time.sleep(0.01)
-		Upath = socket.gethostname() + '/' + ('images' if picture else 'videos') + '/'
-		with open("UploadTimes.txt","a") as file:
-			start_time = time.time()
-			sub.call('aws s3 cp ' + filename + ' s3://pi-1/' + Upath, shell=True)
-			file.write("AWS : "+("Picture" if picture else "Video")+" uploaded in %s seconds on " % (time.time() - start_time) + time.strftime("%Y/%m/%d at %H:%M\n"))
-			#start_time = time.time()
-			#sub.call('/home/pi/Dropbox-Uploader/dropbox_uploader.sh upload ' + filename + ' /' + Upath, sub.call)
-			#file.write("DB  : "+("Picture" if picture else "Video")+" uplaoded in %s seconds on " % (time.time() - start_time) + time.strftime("%Y/%m/%d at %H:%M\n"))
-		with open("./glimpsecam/camera/numFile.txt","w") as numFile:
-			numFile.write(str(int_list[0]) + ' ' + str(int_list[1]))
 	time.sleep(0.01)
 	prevState = currentState
