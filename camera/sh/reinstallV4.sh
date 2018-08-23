6a90fe06763b344bab86055c08e57aaf93e1a740
glimpsewearables <38670739+glimpsewearables@users.noreply.github.com>
2018-08-23 16:16:25 -0700

    Update reinstallV4.sh

--- camera/sh/reinstallV4.sh
+++ camera/sh/reinstallV4.sh
@@ -51,6 +51,7 @@ if [ $ANSWER -eq 2 -o $ANSWER -eq 6 -o $ANSWER -eq 7 ]; then
 	cd pikrellcam
 	chmod u+rwx install-pikrellcam.sh
 	./install-pikrellcam.sh
+	sudo mv ./glimpsecam/camera/sh/pikrellcam.conf ./.pikrellcam/pikrellcam.conf
 fi
 
 # Install AWS CLI
