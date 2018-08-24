d82e33ac5adcf5ee75ad51296a95676f6c56a015
glimpsewearables <38670739+glimpsewearables@users.noreply.github.com>
2018-08-23 20:49:12 -0700

    Update GlimpseCamLowPowerMode.py

--- camera/GlimpseCamLowPowerMode.py
+++ camera/GlimpseCamLowPowerMode.py
@@ -4,6 +4,11 @@ import subprocess as sub
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(5, GPIO.OUT)
+GPIO.output(5, GPIO.HIGH)
+time.sleep(3)
+GPIO.output(5, GPIO.LOW)
+
 held = False
 prevState = False
 
@@ -20,8 +25,12 @@ while True:
 				held = False
 			currentState = not GPIO.input(12)
 		if held:
+			GPIO.output(5, GPIO.HIGH)
+			time.sleep(1)
+			GPIO.output(5, GPIO.LOW)
 			GPIO.cleanup()
-			sub.call('sudo reboot',shell=True)
+			sub.call('python ./glimpsecam/camera/GlimpseCam.py &', shell=True)
+			sub.call('pkill -f ./glimpsecam/camera/GlimpseCamLowPowerMode.py', shell=True)
 	time.sleep(0.01)
 	prevState = currentState
 	#INSERT BACKLOG UPLOADING HERE LATER
