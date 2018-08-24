9ce9fab6541d24866e0e88b43d54dab0589f85c5
glimpsewearables <38670739+glimpsewearables@users.noreply.github.com>
2018-08-23 20:26:52 -0700

    Update GlimpseCam.py

--- camera/GlimpseCam.py
+++ camera/GlimpseCam.py
@@ -45,7 +45,7 @@ time.sleep(3)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 #GPIO.setup(5, GPIO.OUT)
-#GPIO.setup(5, GPIO.HIGH)
+#GPIO.output(5, GPIO.HIGH)
 #time.sleep(1)
 #GPIO.output(5, GPIO.LOW)
 
