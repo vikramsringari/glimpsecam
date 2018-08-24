d609e910e19b8c94c1d975120f1e0c207865cf52
glimpsewearables <38670739+glimpsewearables@users.noreply.github.com>
2018-08-23 19:47:23 -0700

    Update monitor.sh

--- camera/monitor.sh
+++ camera/monitor.sh
@@ -1,8 +1,3 @@
-set -e
-sleep 20
-hostname -I > ip.txt
-python -c 'import geocoder; print geocoder.ip("me").city' >> ip.txt
-aws s3 cp "ip.txt" s3://pi-1/$(hostname)/
 while true
 do
 	gpio -g read 4 > battery.csv
